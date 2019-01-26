e02028434a452f138607e408bfe8d2ac1e039d07
Gold Hat Carls <carlo@itraws.com>
2019-01-26 02:28:35 -0500

    The sites template is almost complete for version 1

---
+++ src/js/sidebar.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("drawer-holder").style.left = "0";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("drawer-holder").style.left = "-250px";
+}
