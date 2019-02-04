1235c61bc1869132e94eb4d719fe5085b8ab7dd2
Gold Hat Carls <carlo@itraws.com>
2019-02-04 11:49:08 -0500

    fixed particles and favicons v1.8

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -2,24 +2,26 @@
   <div>
     <div class="index-gradient-bg-overlay-layer-2" />
 
-    <vue-particles
-      color="#dedede"
-      :particleOpacity="0.7"
-      :particlesNumber="80"
-      shapeType="circle"
-      :particleSize="4"
-      linesColor="#dedede"
-      :linesWidth="1"
-      :lineLinked="true"
-      :lineOpacity="0.4"
-      :linesDistance="150"
-      :moveSpeed="3"
-      :hoverEffect="true"
-      hoverMode="grab"
-      :clickEffect="true"
-      clickMode="push"
-      class="index-particles-bg-overlay"
-    />
+    <no-ssr>
+      <vue-particles
+        color="#dedede"
+        :particleOpacity="0.7"
+        :particlesNumber="80"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#dedede"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+        class="index-particles-bg-overlay"
+      />
+    </no-ssr>
     <div class="index-gradient-bg-overlay" />
     <div class="container-fluid">
       <nav-bar />
--- nuxt.config.js
+++ nuxt.config.js
@@ -23,15 +23,48 @@ module.exports = {
         content: pkg.description
       },
       {
+        name: 'msapplication-TileColor',
+        content: '#da532c'
+      },
+      {
+        name: 'theme-color',
+        content: '#ffffff'
+      },
+      {
         hid: 'keywords',
         name: 'keywords',
         content: 'vuejs, nuxt, javascript, tutorials, development, software'
       }
     ],
     link: [{
+      rel: 'apple-touch-icon',
+      size: '180x180',
+      href: '/apple-touch-icon.png'
+      },{
+        rel: 'icon',
+        type: 'image/png',
+        size: '32x32',
+        href: '/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        size: '16x16',
+        href: '/favicon-16x16.png'
+      },
+      {
         rel: 'icon',
         type: 'image/x-icon',
         href: '/favicon.ico'
+      },
+      {
+        rel: 'manifest',
+        href: '/site.webmanifest'
+      },
+      {
+        rel: 'mask-icon',
+        href: '/safari-pinned-tab.svg',
+        color: '#5bbad5'
       },
       {
         rel: 'stylesheet',
